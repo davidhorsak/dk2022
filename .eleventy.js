19ed9b09442d01e4c10ef39352d59e439ebd25d6
David Horsak <david.horsak@outlook.com>
2022-04-15 22:13:29 +0200

    build to public folder

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output: "dist"
+            output: "public"
         },
     };
 };
