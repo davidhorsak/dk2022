11b61cd597e39f0c63f49397d048205ba7d80a63
David Horsak <david.horsak@outlook.com>
2022-04-15 22:00:16 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css/*.css");
+    eleventyConfig.addPassthroughCopy("./src/js/*.js");
+    eleventyConfig.addPassthroughCopy("./src/dist/*.js");
+    eleventyConfig.addPassthroughCopy("./src/images/*");
+    eleventyConfig.addPassthroughCopy("./src/fonts/**/*");
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        },
+    };
+};
