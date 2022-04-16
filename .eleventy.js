366fb00faf3fb3c0b7da02f5bb0640c3364fa55e
David Horsak <david.horsak@outlook.com>
2022-04-16 19:05:44 +0200

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,25 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/*.css");
   eleventyConfig.addPassthroughCopy("./src/js/*.js");
   eleventyConfig.addPassthroughCopy("./src/dist/*.js");
   eleventyConfig.addPassthroughCopy("./src/images/*");
   eleventyConfig.addPassthroughCopy("./src/fonts/**/*");
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if(outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
