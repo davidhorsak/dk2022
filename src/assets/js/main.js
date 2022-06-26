f906d70f0bf5e1a6ebc843fea713130adef1fab9
David Horsak <david.horsak@outlook.com>
2022-06-26 13:58:58 +0200

    correct start time

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Aug 12, 2022 17:00:00").getTime();
+	var countDownDate = new Date("Aug 12, 2022 15:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
